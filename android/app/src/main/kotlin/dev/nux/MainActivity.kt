d2dc2073b840b66aa2d0985e01ae1cfe2ee1ec12
LUSEDOU <dolorier_uribe@hotmail.com>
2024-05-06 20:19:17 -0500

    template

---
+++ android/app/src/main/kotlin/dev/nux/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.nux.jobmini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:jobmini/app/app.dart';
+import 'package:jobmini/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
